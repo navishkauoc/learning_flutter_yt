d63367a3692f1f34b34fcf1780818d5a3fc40661
navishkauoc <navishka.randika@gmail.com>
2021-06-12 23:12:07 +0530

    Safe area widget added

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home screen'),
+      body: SafeArea(child: Text('Home screen')),
     );
   }
 }
